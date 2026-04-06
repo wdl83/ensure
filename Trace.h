72ac078bd817687af9d9baeb07f58f4a7f4aefe9
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2026-04-06 09:41:49 +0200

    Added clang-format + reformated.

--- Trace.h
+++ Trace.h
@@ -6,7 +6,7 @@
 #include <string>
 
 #ifndef _GNU_SOURCE
-#define _GNU_SOURCE
+    #define _GNU_SOURCE
 #endif
 
 #include <unistd.h>
@@ -23,12 +23,15 @@ enum class TraceLevel
     end
 };
 
-inline
-const char* toString(TraceLevel level)
+inline const char *toString(TraceLevel level)
 {
     static const char *str[] = {"E", "W", "I", "D", "T"};
-    static_assert(sizeof(str) / sizeof(str[0]) == int(TraceLevel::end) - int(TraceLevel::begin));
-    return TraceLevel::end > level && TraceLevel::begin <= level ?  str[int(level)] : "?";
+    static_assert(
+        sizeof(str) / sizeof(str[0])
+        == int(TraceLevel::end) - int(TraceLevel::begin));
+    return TraceLevel::end > level && TraceLevel::begin <= level
+        ? str[int(level)]
+        : "?";
 }
 
 using LogLevel = TraceLevel;
@@ -41,48 +44,41 @@ void traceSerializeImpl(std::ostream &os, const T &value)
     os << value;
 }
 
-inline
-void traceImpl(std::ostream &)
-{}
+inline void traceImpl(std::ostream &) { }
 
-template <typename T, typename ...T_n>
+template <typename T, typename... T_n>
 void traceImpl(std::ostream &os, const T &value, const T_n &...tail)
 {
     traceSerializeImpl(os, value);
     traceImpl(os, tail...);
 }
 
-inline
-TraceLevel currTraceLevel()
+inline TraceLevel currTraceLevel()
 {
     static const char *env = ::getenv("TRACE_LEVEL");
-    static const TraceLevel level =
-        env ? static_cast<TraceLevel>(::atoi(env)) : TraceLevel::Info;
+    static const TraceLevel level
+        = env ? static_cast<TraceLevel>(::atoi(env)) : TraceLevel::Info;
     return level;
 }
 
-} /* impl_ */
+} // namespace impl_
 
-
-inline
-void trace(TraceLevel traceLevel, const std::ostringstream &oss)
+inline void trace(TraceLevel traceLevel, const std::ostringstream &oss)
 {
-    if(int(impl_::currTraceLevel()) < int(traceLevel)) return;
+    if (int(impl_::currTraceLevel()) < int(traceLevel)) return;
 
-    std::ostream &dst =
-        TraceLevel::Error == traceLevel || TraceLevel::Warning == traceLevel
-        ? std::cerr : std::cout;
-    dst << '['
-        << ::getpid() << '|'
-        << ::gettid() << '|'
-        << toString(traceLevel) << "] "
-        << oss.str() << std::endl;
+    std::ostream &dst
+        = TraceLevel::Error == traceLevel || TraceLevel::Warning == traceLevel
+        ? std::cerr
+        : std::cout;
+    dst << '[' << ::getpid() << '|' << ::gettid() << '|' << toString(traceLevel)
+        << "] " << oss.str() << std::endl;
 }
 
-template <typename ...T_n>
+template <typename... T_n>
 void trace(TraceLevel traceLevel, const T_n &...tail)
 {
-    if(int(impl_::currTraceLevel()) < int(traceLevel)) return;
+    if (int(impl_::currTraceLevel()) < int(traceLevel)) return;
 
     std::ostringstream ss;
 
@@ -90,32 +86,32 @@ void trace(TraceLevel traceLevel, const T_n &...tail)
     trace(traceLevel, ss);
 }
 
-
 #ifndef ENABLE_TRACE
 
-#define TRACE(...)
+    #define TRACE(...)
 
 #else
 
-#define TRACE(traceLevel, ...) \
-    do \
-    { \
-        trace(traceLevel, __FILE__, ':', __LINE__, ' ', __FUNCTION__, ' ', __VA_ARGS__); \
-    } while(false)
+    #define TRACE(traceLevel, ...)                                             \
+        do                                                                     \
+        {                                                                      \
+            trace(                                                             \
+                traceLevel, __FILE__, ':', __LINE__, ' ', __FUNCTION__, ' ',   \
+                __VA_ARGS__);                                                  \
+        } while (false)
 
 #endif /* ENABLE_TRACE */
 
-
 #ifndef ENABLE_LOG
 
-#define LOG(...)
+    #define LOG(...)
 
 #else
 
-#define LOG(logLevel, ...) \
-    do \
-    { \
-        trace(logLevel, __VA_ARGS__); \
-    } while(false)
+    #define LOG(logLevel, ...)                                                 \
+        do                                                                     \
+        {                                                                      \
+            trace(logLevel, __VA_ARGS__);                                      \
+        } while (false)
 
 #endif /* ENABLE_TRACE */
